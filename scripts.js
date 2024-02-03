93338b704f4f63cd9bfe15598f89428e30b5b2cf
Vinicios Lacerda <viniedujl@gmail.com>
2024-02-03 18:54:25 -0300

    initial commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+var elements = document.querySelectorAll('.duvida')
+
+elements.forEach(function (duvida) {
+    duvida.addEventListener('click', function () {
+        duvida.classList.toggle('ativa')
+    })
+})
